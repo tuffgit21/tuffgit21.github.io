f62c9a14e7c883ccf159be009acd82183063f04f
Youssef <youssefmourad1945@gmail.com>
2026-06-29 11:23:59 +0300

    Updated the installer to version 0.2

--- EPK_installer.py
+++ EPK_installer.py
@@ -1,3 +1,10 @@
 import urllib.request
+import sys
+import time
+for i in range(101):
+    sys.stdout.write("\rDownloading EPK... {}%".format(i))
+    sys.stdout.flush()
+    time.sleep(1)
+print("\nEPK downloaded!")
 url = "https://tuffgit21.github.io/EPK.zip"
 urllib.request.urlretrieve(url, "EPK.zip")
