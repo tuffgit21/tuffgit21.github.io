d5c4c169aae5d10e6c816a452c6542bb430669dd
tuffgit21 <94118843+tuffgit21@users.noreply.github.com>
2026-07-21 15:20:32 +0300

    Update EPK_installer.py

--- EPK_installer.py
+++ EPK_installer.py
@@ -1,10 +1,19 @@
-import urllib.request
 import sys
-import time
-for i in range(101):
-    sys.stdout.write("\rDownloading EPK... {}%".format(i))
-    sys.stdout.flush()
-    time.sleep(1)
-print("\nEPK downloaded!")
+import urllib.request
+
 url = "https://tuffgit21.github.io/EPK.zip"
-urllib.request.urlretrieve(url, "EPK.zip")
+filename = "EPK.zip"
+
+
+def report_progress(block_num, block_size, total_size):
+    downloaded = block_num * block_size
+    if total_size <= 0:
+        percent = 0
+    else:
+        percent = min(100, int(downloaded * 100 / total_size))
+    sys.stdout.write(f"\rDownloading EPK... {percent}%")
+    sys.stdout.flush()
+
+
+urllib.request.urlretrieve(url, filename, reporthook=report_progress)
+print("\nEPK downloaded!")
